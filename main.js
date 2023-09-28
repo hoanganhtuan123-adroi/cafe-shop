2b3429cde7385d6ab4e912ed47d1c42c5ff9bb40
hoanganhtuan123-adroi <tuanhoang2801@gmail.com>
2023-09-28 20:35:02 +0700

    Second Change

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
-const btnOpenSignin = document.querySelector(".js-sign-in");
-const btnOpenSignup = document.querySelector(".js-sign-up");
-const btnCloseModal = document.querySelector(".js-closeModal");
-const modal = document.querySelector(".modal");
-
-const alerText = document.querySelector(".modal__btn");
-
-alerText.addEventListener("click", function () {
-    alert("You have successfully login!");
+// Overlay
+const overlayModal = document.querySelector(".overlay");
+const signInModal = document.querySelector(".signIn-modal");
+document.addEventListener("keydown", function (e) {
+    if (
+        e.key === "Escape" &&
+        !modalAbout.classList.contains("hidden") &&
+        !overlayModal.classList.contains("hidden")
+    ) {
+        closeAboutUs();
+    }
 });
 
-const openModal = function () {
-    modal.classList.remove("hidden");
-};
+// Sign in
+const btnOpenSignIn = document.querySelector(".js-sign-in");
+const btnCloseModal = document.querySelector(".js-closeSignIn");
 
-btnOpenSignup.addEventListener("click", openModal);
+const openModal = function () {
+    signInModal.classList.remove("hidden");
+    overlayModal.classList.remove("hidden");
+};
 
 const closeModal = function () {
-    modal.classList.add("hidden");
+    signInModal.classList.add("hidden");
+    overlayModal.classList.add("hidden");
 };
+
+btnOpenSignIn.addEventListener("click", openModal);
 
 btnCloseModal.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    if (e.key === "Escape" && !signInModal.classList.contains("hidden")) {
         closeModal();
     }
 });
+
+// About
+const openAbout = document.querySelector(".js-about-btn");
+const modalAbout = document.querySelector(".about-modal");
+const closeAbout = document.querySelector(".js-closeAbout");
+
+function btnOpenAboutUs() {
+    modalAbout.classList.remove("hidden");
+    overlayModal.classList.remove("hidden");
+}
+
+openAbout.addEventListener("click", btnOpenAboutUs);
+
+function closeAboutUs() {
+    modalAbout.classList.add("hidden");
+    overlayModal.classList.add("hidden");
+}
+
+closeAbout.addEventListener("click", closeAboutUs);
